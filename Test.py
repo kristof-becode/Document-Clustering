9274bcb3a4cb66fba41ae6226b5e0a51884cad72
becode <kristof.mantoray@gmail.com>
2020-12-06 19:06:25 +0100

    Delete folder

--- Test.py
+++
@@ -1,8 +0,0 @@
-import nltk
-from nltk.tokenize import sent_tokenize, word_tokenize, RegexpTokenizer
-from nltk.corpus import stopwords
-#nltk.download('stopwords')
-stop_words = set(stopwords.words('english'))
-
-print(stop_words)
-print(len(stop_words))
